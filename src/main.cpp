dc059530e34e9b213858f7658c53a8821b76204a
DrUnicornIT <congcongcong6e@gmail.com>
2022-04-27 07:33:22 +0700

    Change main.cpp - newgame

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,10 @@
 #include <ctime>
 #include <cmath>
 #include <stdlib.h>
-
-#include "player.h"
-#include "entity.h"
+#include "CommonFunction.h"
 #include "renderwindow.h"
-#include "ground.h"
-
+#include "resourceHandler.h"
+#include "Game.h"
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 800;
 
@@ -21,13 +19,7 @@ bool init();
 bool loadMedia();
 bool runGame();
 
-RenderWindow window;
-SDL_Texture* groundTexture[4];
-std::vector<SDL_Texture*> player_Dead;
-std::vector<SDL_Texture*> player_Idle;
-std::vector<SDL_Texture*> player_Jump;
-std::vector<SDL_Texture*> player_Run;
-std::vector<SDL_Texture*> player_Walk;
+
 bool init()
 {
 	bool success = true;
@@ -66,93 +58,34 @@ bool init()
 }
 
 
-
+ResHandler resourceHandler;
+Game game;
 bool loadMedia()
 {
 	bool success = true;
 	window.create("OwOGoGo", SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	groundTexture[0] = window.loadTexture("img/short.png");
-	groundTexture[1] = window.loadTexture("img/medium.png");
-	groundTexture[2] = window.loadTexture("img/long.png");
-	groundTexture[3] = window.loadTexture("img/hole.png");
-	for (int i = 1; i <= 30; i++) {
-		player_Dead.push_back(window.loadTexture("img/player/female/Dead (" + std::to_string(i) + ").png"));
-	}
-
-	for (int i = 1; i <= 16; i++) {
-		player_Idle.push_back(window.loadTexture("img/player/female/Idle (" + std::to_string(i) + ").png"));
-	}
-
-	for (int i = 1; i <= 30; i++) {
-		player_Jump.push_back(window.loadTexture("img/player/female/Jump (" + std::to_string(i) + ").png"));
-	}
-
-	for (int i = 1; i <= 20; i++) {
-		player_Run.push_back(window.loadTexture("img/player/female/Run (" + std::to_string(i) + ").png"));
-	}
-
-	for (int i = 1; i <= 20; i++) {
-		player_Walk.push_back(window.loadTexture("img/player/female/Walk (" + std::to_string(i) + ").png"));
-	}
-	SDL_Delay(1000);
+	resourceHandler.load();
+	std::cout << "Loadmedia done.";
+	SDL_Delay(100);
 	return success;
 }
 
 bool Load = loadMedia();
 
-Player player_Female(player_Dead, player_Idle, player_Jump, player_Run, player_Walk);
-Ground ground(groundTexture[0], groundTexture[1], groundTexture[2], groundTexture[3]);
-
-bool runGame()
-{
-	bool success = true;
-
-	SDL_Event event;
-	bool quit = false;
-	float newHeghtGround = SCREEN_HEIGHT - 350.0f;
-	float gravity = 0.0f;
-
-	while (!quit) {
-		while (SDL_PollEvent(&event) != 0) {
-			switch (event.type) {
-			case SDL_QUIT: {
-				quit = true;
-				break;
-			}
-			}
-			player_Female.handleEvent(event);
-		}
-		ground.update(50, newHeghtGround, gravity);
-		player_Female.update(ground);
-		window.clear();
-		/// 0 easy, 50 normal, 100 hard
-
-		for (int i = 0; i < ground.getLength(); i++) {
-			window.render(ground.getTile(i));
-		}
-		for (int i = 0; i < player_Female.getLength(); i++) {
-			window.render(player_Female.getTile(i));
-		}
-		window.display();
-		SDL_Delay(16);
-	}
-	return success;
-}
-
 int main(int argc, char* args[])
 {
 	srand((unsigned)time(0));
+	/// seed random number generator
 	std::cout << "Start running game.\n";
 	if (init()) {
 		std::cout << "Game init success.\n";
 		if (Load) {
 			std::cout << "Game load media success.\n";
-			if (runGame()) {
-				std::cout << "Game run success.\n";
+			if (game.RunGame()) {
+				std::cout << "Game runned success.\n";
 			}
 			else {
-				std::cout << "Game run failed.\n";
+				std::cout << "Game runned failed.\n";
 			}
 		}
 		else {
